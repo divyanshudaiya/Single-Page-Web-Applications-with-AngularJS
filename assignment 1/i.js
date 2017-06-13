ece1bd47ce3f2452155e4e3e32b5d788c7b89bfe
Divyanshu Daiya <acommonmanrises@gmail.com>
2017-06-13 09:32:42 +0530

    'solution'

---
+++ assignment 1/i.js
@@ -0,0 +1,37 @@
+(function(){
+		
+
+	angular.module('LunchCheck',[])
+	.controller('LunchCheckController',LunchCheckController);
+	LunchCheckController.$inject=['$scope'];
+	function LunchCheckController($scope){
+		$scope.customStyle={};
+		$scope.name="";
+		$scope.curl="Please enter your food choices in text box :)";
+		$scope.checkeri=function(){
+			var t=fui($scope.name);
+			$scope.curl=t;
+			if (t==="Enjoy!"){
+				$scope.customStyle.style={"background-color":"green"};
+			}
+			else if(t==="Too much!"){
+				$scope.customStyle.style={"background-color":"red"};
+			}
+		};
+		function fui(foe){
+			var res = foe.split(",");
+			if (res.length>3){
+				return "Too much!";
+			}
+			else if (res.length==0) {
+				return "Please enter data first";
+			}
+			else{
+				return "Enjoy!";
+			}
+
+		}
+	}
+	
+	
+})();
