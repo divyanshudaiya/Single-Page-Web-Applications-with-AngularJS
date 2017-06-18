a6f61c1b9ff35ce6e14c6380a5e0544bba942c7d
Divyanshu Daiya <acommonmanrises@gmail.com>
2017-06-18 21:00:59 +0530

    final

--- assignment 4/categories.component.js
+++ assignment 4/categories.component.js
@@ -6,7 +6,7 @@ angular.module('data')
 
 .component('categories',{
 	template:'<ul><li ng-repeat="item in $ctrl.catitems" >'+
-				'<a ui-sref="categorie.itemi({categoryShortname: item.short_name})">{{ item.short_name }} of {{ item.name }}</a></li></ul>',
+				'<a ui-sref="categorie.itemi({categoryShortname: item.short_name})">{{ item.short_name }} for {{ item.name }}</a></li></ul>',
 	bindings:{
 		catitems:'<'
 	}
--- assignment 4/items.component.js
+++ assignment 4/items.component.js
@@ -5,7 +5,7 @@
 angular.module('data')
 .component('items',{
 	template:'<ul><li ng-repeat="itemt in $ctrl.items">'+
-			'{{ itemt.short_name }} of {{ itemt.name }}</li></ul>',
+			'{{ itemt.short_name }}  {{ itemt.name }}</br><p style="font-size:0.8em; color:yellow;">{{itemt.description}}</p></li></ul>',
 	bindings:{
 		items:'<'
 	}
